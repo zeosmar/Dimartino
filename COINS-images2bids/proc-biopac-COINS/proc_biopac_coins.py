fddb05721a8d105f2d7b93586f4d1db0f7a990b1
ajb17 <anthony.beetem@gmail.com>
2019-02-14 16:49:13 -0500

    Add 'COINS-images2bids/' from commit '62510c13d2b772f8e02c2622b02060ee2cc85683'
    git-subtree-dir: COINS-images2bids
    git-subtree-mainline: 62510c13d2b772f8e02c2622b02060ee2cc85683
    git-subtree-split: 62510c13d2b772f8e02c2622b02060ee2cc85683

---
+++ COINS-images2bids/proc-biopac-COINS/proc_biopac_coins.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 21 10:25:37 2018
+
+@author: stan
+"""
+import os
+import argparse
+import sys
+import os.path as path
+from physio_libs import * 
+
+class MyParser(argparse.ArgumentParser):
+    def error(self, message):
+        sys.stderr.write('error: %s\n' % message)
+        self.print_help()
+        sys.exit(2)
+        
+parser = MyParser(prog="proc_biopac_coins")
+parser.add_argument('-project','--project_directory', help="The project folder containing sourceData, etc. (Required)", required=True)
+parser.add_argument("-sublist", "--subject_list", help="File name listing subjects to process; defaults to 'subject_list.txt' in the working directory.", default='subject_list.txt')
+parser.add_argument("-templates", "--template_directory", help="Directory for physio templates; defaults to 'physio_templates' in project dir", default='physio_templates')
+args = parser.parse_args()
+
+project_dir = path.abspath(path.expanduser(args.project_directory))
+if args.template_directory == "physio_templates":
+    template_dir = path.join(project_dir,"physio_templates")
+else:
+    template_dir = path.abspath(path.expanduser(project_dir))
+    
+sublist = [fil.strip() for fil in open(args.subject_list)]
+
+for sub in sublist:
+    print(sub.upper().strip())
+    physio = PhysioObject()
+    physio.subid = sub.strip()
+    physio.project_directory = project_dir
+    sub_template = path.join(template_dir, "%s_physio-template.json"%sub)
+    physio.load_from_template(sub_template)
+    qa_dir = os.path.join(project_dir, sub,'QC','physio')
+    plot_subject_struct(physio, qa_dir)
+    physio_out = os.path.join(project_dir,"BIDS")
+    save_physio_csv(physio, project_dir)
+    save_physio_tsv(physio, physio_out)
+    print('')
+    
